b54e37e47b765b54715d832ab24ea61f47d86518
gianni0510 <85133039+gianni0510@users.noreply.github.com>
2021-06-03 16:05:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req,res,next)=>{
-  res.send("important information from my computer")
+  res.send("test file")
 })
 //heroku injects the port number into the PORT env value
 const port = process.env.PORT || 3001;
